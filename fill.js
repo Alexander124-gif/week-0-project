49c3a794e401df2c4800725e2e7ff9450ccf6101
Alexander124-gif <ezeokeemmanuel88@gmail.com>
2025-01-21 00:12:07 +0000

    fill.js

---
+++ fill.js
@@ -0,0 +1,3 @@
+//this is a project for week 0 of the course. 
+//it is a simple project that introduces the student to the basics of using github.
+//the project is to store multiple codes  that the user writes for a certain projects or different project
